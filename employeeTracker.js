71455db7effbf1976cf93123938501f5565d3dd1
William James <Williamj3795@gmail.com>
2022-11-10 22:21:10 -0500

    connection for mysql

--- employeeTracker.js
+++ employeeTracker.js
@@ -6,3 +6,11 @@ const inquirer = require('inquirer');
 require('dotenv').config();
 // print MySQL rows to the console.
 require('console.table');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'employeeTracker_db'
+});
