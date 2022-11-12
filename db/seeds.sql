61791813541b4d4f8373a19fc28c56427de79ed1
William James <Williamj3795@gmail.com>
2022-11-12 09:56:37 -0500

    INSERT INTO role and VALUES

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,3 +10,11 @@ VALUES
 
 INSERT INTO role 
 (title, salary, department_id)
+
+VALUES
+('Sales Manager', 60000, 1),
+('Sales Associate', 40000, 1),
+('Engineering Manager', 75000, 2),
+('Engineering Associate', 55000, 2),
+('Marketing Manager', 85000, 3),
+('Marketing Associate', 650000, 3);
