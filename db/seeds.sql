3cb54f9f78c728365052341149a8e6d74928eb29
William James <Williamj3795@gmail.com>
2022-11-12 11:56:46 -0500

    insert into employee and values

--- db/seeds.sql
+++ db/seeds.sql
@@ -18,3 +18,14 @@ VALUES
 ('Engineering Associate', 55000, 2),
 ('Marketing Manager', 85000, 3),
 ('Marketing Associate', 650000, 3);
+
+INSERT INTO employee
+(first_name, last_name, role_id, manager_id)
+
+VALUES
+('Miles', 'Morales', 1, 2),
+('Peter', 'Parker', 2, 1),
+('Tony', 'Stark', 3, 4),
+('James', 'Rhoades', 4, 3),
+('Steve', 'Rodgers', 5, 6),
+('Bucky', 'Barnes', 6, 5);
